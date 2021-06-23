3de28d99aa211aeadfab4cd23f3b339a305f97b9
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-06-22 22:36:21 -0300

    fix: aplica linter no exercício

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = { searchDigimon: '', isFetching: false, errorMessage: '' };
+
+    this.inputValue = this.inputValue.bind(this);
+    this.requestDigimon = this.requestDigimon.bind(this);
   }
 
-  inputValue = (value) => {
+  inputValue(value) {
     this.setState((state) => ({
       ...state,
       searchDigimon: value,
     }));
   }
 
-  requestDigimon = async () => {
+  async requestDigimon() {
     const { searchDigimon } = this.state;
     if (searchDigimon) {
       fetch(`https://digimon-api.vercel.app/api/digimon/name/${searchDigimon}`)
@@ -23,7 +26,7 @@ class App extends React.Component {
         .then((results) => this.setState((state) => ({
           ...state,
           digimon: results[0],
-          errorMessage: results["ErrorMsg"],
+          errorMessage: results.errorMessage,
           isFetching: true,
         })));
     }
@@ -34,21 +37,22 @@ class App extends React.Component {
     return (
       <div className="App">
         <input
-          value={searchDigimon}
+          value={ searchDigimon }
           type="text"
-          onChange={({ target }) => this.inputValue(target.value)}
+          onChange={ ({ target }) => this.inputValue(target.value) }
           data-testid="input"
         />
         <button
           data-testid="buttonSearch"
-          onClick={this.requestDigimon}
+          onClick={ this.requestDigimon }
           type="button"
         >
           Search Digimon
         </button>
-        { isFetching && !errorMessage
-          ? <Digimon digimon={digimon} />
-          : <h1>{errorMessage || 'Faça uma pesquisa'}</h1>
+        {
+          isFetching && !errorMessage
+            ? <Digimon digimon={ digimon } />
+            : <h1>{errorMessage || 'Faça uma pesquisa'}</h1>
         }
       </div>
     );
--- src/Digimon.js
+++ src/Digimon.js
@@ -4,14 +4,17 @@ import PropTypes from 'prop-types';
 const Digimon = ({ digimon }) => (
   <div>
     <h2 data-testid="digimonName">{digimon.name}</h2>
-    <p data-testid="digimonLevel">level: {digimon.level}</p>
-    <img src={digimon.img} alt={digimon.name} />
+    <p data-testid="digimonLevel">
+      level:
+      {digimon.level}
+    </p>
+    <img src={ digimon.img } alt={ digimon.name } />
   </div>
 );
 
 export default Digimon;
 
-Digimon.prototype = PropTypes.shape({
+Digimon.propTypes = PropTypes.shape({
   name: PropTypes.string,
   level: PropTypes.number,
   img: PropTypes.string,
